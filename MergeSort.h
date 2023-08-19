591e8351c92e7c8e004926e205addc802186f187
Greg-Farakhutdinov <141477929+Greg-Farakhutdinov@users.noreply.github.com>
2023-08-19 11:04:28 +0500

    Create MergeSort.h

---
+++ MergeSort.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include <chrono>
+#include <ctime>
+#include <random>
+#include <future>
+
+using namespace std;
+
+void print_cout(string);
+
+void merge(vector <int>&, int, int);
+
+void MergeSort(vector <int>&, int, int);
+
+void MergeSortThread(vector<int>& arr, int first, int last);
+
+void MergeSortOne(vector<int>&, int, int);
